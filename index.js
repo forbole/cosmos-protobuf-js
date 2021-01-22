a0187dd1beebd195b11d45939fcde0d9cd60168a
Kwun Yeung <kwun@forbole.com>
2021-01-22 16:40:56 +0800

    export all pb

--- index.js
+++ index.js
@@ -1,5 +1,61 @@
 // x/auth
 export * from 'protobuf/cosmos/auth/v1beta1/auth_pb'
 export * from 'protobuf/cosmos/auth/v1beta1/genesis_pb'
-export * from 'protobuf/cosmos/auth/v1beta1/query'
+export * from 'protobuf/cosmos/auth/v1beta1/query_pb'
 
+// x/bank
+export * from 'protobuf/cosmos/bank/v1beta1/bank_pb'
+export * from 'protobuf/cosmos/bank/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/bank/v1beta1/query_pb'
+export * from 'protobuf/cosmos/bank/v1beta1/tx_pb'
+
+// x/crisis
+export * from 'protobuf/cosmos/crisis/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/crisis/v1beta1/tx_pb'
+
+// x/distribution
+export * from 'protobuf/cosmos/distribution/v1beta1/distribution_pb'
+export * from 'protobuf/cosmos/distribution/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/distribution/v1beta1/query_pb'
+export * from 'protobuf/cosmos/distribution/v1beta1/tx_pb'
+
+// x/evidence
+export * from 'protobuf/cosmos/evidence/v1beta1/evidence_pb'
+export * from 'protobuf/cosmos/evidence/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/evidence/v1beta1/query_pb'
+export * from 'protobuf/cosmos/evidence/v1beta1/tx_pb'
+
+// x/gov
+export * from 'protobuf/cosmos/gov/v1beta1/gov_pb'
+export * from 'protobuf/cosmos/gov/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/gov/v1beta1/query_pb'
+export * from 'protobuf/cosmos/gov/v1beta1/tx_pb'
+
+// x/mint
+export * from 'protobuf/cosmos/mint/v1beta1/mint_pb'
+export * from 'protobuf/cosmos/mint/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/mint/v1beta1/query_pb'
+
+// x/params
+export * from 'protobuf/cosmos/params/v1beta1/params_pb'
+export * from 'protobuf/cosmos/params/v1beta1/query_pb'
+
+// x/slashing
+export * from 'protobuf/cosmos/slashing/v1beta1/slashing_pb'
+export * from 'protobuf/cosmos/slashing/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/slashing/v1beta1/query_pb'
+export * from 'protobuf/cosmos/slashing/v1beta1/tx_pb'
+
+// x/staking
+export * from 'protobuf/cosmos/staking/v1beta1/staking_pb'
+export * from 'protobuf/cosmos/staking/v1beta1/genesis_pb'
+export * from 'protobuf/cosmos/staking/v1beta1/query_pb'
+export * from 'protobuf/cosmos/staking/v1beta1/tx_pb'
+
+// x/upgrade
+export * from 'protobuf/cosmos/upgrade/v1beta1/upgrade_pb'
+export * from 'protobuf/cosmos/upgrade/v1beta1/query_pb'
+
+// tx
+export * from 'protobuf/cosmos/upgrade/v1beta1/service_pb'
+export * from 'protobuf/cosmos/upgrade/v1beta1/tx_pb'
